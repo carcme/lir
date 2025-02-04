10226dc5c759211008191dacbbaf31a68c8e46f1
carc <carcmedev@gmail.com>
2025-02-04 13:26:03 +0100

    added onLoad for map; added method for form post (durr!)

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import helmetData from "@/json/helmetData";
 import { useLanguage, getLanguage } from "../context/LanguageContext";
 
 const Contact = () => {
+  const [mapLoaded, setMapLoaded] = useState(false);
   const meta = getLanguage(helmetData);
 
+  const onMapLoaded = () => {
+    setMapLoaded(true);
+  };
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -35,8 +39,13 @@ const Contact = () => {
         <meta name="google" content={meta.google} />
       </Helmet>
       <div className="page">
-        <section class="text-gray-600 body-font relative">
-          <div class="absolute inset-0 bg-gray-300">
+        <section class="text-primaryGreen body-font relative">
+          <div class="absolute inset-0 bg-gray-200 ">
+            {!mapLoaded && (
+              <h2 className="absolute text-4xl inset-0 top-[6%] md:right-[50%] lg:right-0 md:top-1/2 font-semibold justify-center text-center">
+                Loading Map
+              </h2>
+            )}
             <iframe
               width="100%"
               height="100%"
@@ -46,9 +55,11 @@ const Contact = () => {
               title="map"
               scrolling="no"
               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9710.926251435141!2d13.3340262!3d52.5201969!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a85109d53aa221%3A0x76f0488133aba72!2sThe%20Lir!5e0!3m2!1sen!2sde!4v1738666779264!5m2!1sen!2sde"
+              onLoad={() => onMapLoaded()}
             ></iframe>
-          </div>
-          <form action="POST" data-netlify="true">
+          </div>{" "}
+          data-netlify="true"
+          <form action="POST" method="POST" data-netlify="true">
             <div class="container px-5 py-24 mx-auto flex">
               <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
                 <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
@@ -87,7 +98,10 @@ const Contact = () => {
                     class="w-full bg-white rounded border border-primaryGreen focus:border-primaryGreen focus:ring-1 focus:ring-primaryGreen h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                   ></textarea>
                 </div>
-                <button class="text-white bg-primaryGreen border py-2 px-6 focus:outline-none hover:bg-white hover:text-primaryGreen  border-primaryGreen rounded text-lg">
+                <button
+                  class="text-white bg-primaryGreen border py-2 px-6 focus:outline-none hover:bg-white hover:text-primaryGreen  border-primaryGreen rounded text-lg transition-colors duration-300 ease-in-out"
+                  type="submit"
+                >
                   Send
                 </button>
                 <p class="text-xs font-light text-gray-500 mt-3">
